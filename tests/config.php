7d74e4113c0e8f0d528dd20a1dc4044b3ca665c4
Joseff Indradjaja <joseff@hotmail.com>
2023-06-16 10:09:53 +0700

    Added Pcare Revised tests with autoloader

--- tests/config.php
+++ tests/config.php
@@ -6,3 +6,13 @@ $vclaim_conf = [
     'base_url'      => 'https://apijkn-dev.bpjs-kesehatan.go.id',
     'service_name'  => 'vclaim-rest-dev'
 ];
+
+$pcare_conf = [
+    'cons_id'       => '123456',
+    'secret_key'    => 'abcdef',
+    'user_key'      => '6bbfb9e9b535cd9c32bd77451050c6a0',
+    'base_url'      => 'https://apijkn-dev.bpjs-kesehatan.go.id',
+    'service_name'  => 'pcare-rest-dev',
+    'pcare_user'    => 'mimin',
+    'pcare_pass'    => 'password',
+];
